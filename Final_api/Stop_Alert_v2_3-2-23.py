9d178704a5fc18f67c565589d3c156bea469509c
osamali0301 <131901571+osamali0301@users.noreply.github.com>
2024-01-03 16:39:09 +0500

    Create Stop_Alert_v2_3-2-23.py

---
+++ Final_api/Stop_Alert_v2_3-2-23.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI, Request, Response
+import json
+import requests
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {"message": "WebEngage Ride Stop Alert Application Started"}
+
+
+@app.post("/end_point1")
+async def end_point1(request: Request):
+
+    try:
+        body = await request.body()
+        list_items = json.loads(body)
+        values_list = list_items.split(',')
+        for value in values_list:
+            header1 = {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer a44f5d93-4f06-4d6f-873b-e96d7945e48f"
+            }
+            data = {
+                "userId": str(value),
+                "eventName": "stop_alert_sent",
+                "eventData": {}
+            }
+            response = requests.post('https://api.webengage.com/v1/accounts/~2024b707/events', headers=header1, json=data)
+
+            print(value)
+            print(response.json())
+
+        return (response.json())
+
+    except Exception as ep1_exc:
+        print("Exception: " + str(ep1_exc))
+
+
+
